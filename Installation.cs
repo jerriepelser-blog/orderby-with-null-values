f39d0b49486af81291342e23a35c4daceeca78ec
Jerrie Pelser <jerrie@jerriepelser.com>
2020-02-19 14:30:06 +0700

    Initial commit

---
+++ Installation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OrderByWithNull
+{
+    public class Installation
+    {
+        public int Id { get; set; }
+
+        public string Customer { get; set; }
+
+        public DateTime? InstallationDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Bogus;
+using Bogus.Extensions;
+using ConsoleTables;
+
+namespace OrderByWithNull
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var installations = new Faker<Installation>()
+                .UseSeed(1)
+                .RuleFor(o => o.Id, faker => faker.IndexFaker)
+                .RuleFor(o => o.InstallationDate, faker => faker.Date.Future().OrNull(faker))
+                .RuleFor(o => o.Customer, faker => faker.Person.FullName)
+                .Generate(10);
+
+            var orderedInstallations = installations
+                .OrderByDescending(i => i.InstallationDate.HasValue)
+                .ThenBy(i => i.InstallationDate);
+            
+            ConsoleTable.From(orderedInstallations)
+                .Write(Format.Alternative);
+        }
+    }
+}
